ec365b52769dc862399746c3dc76f8737da3b41a
Meixuan Chen <ellatudor1850@gmail.com>
2024-09-27 21:22:33 -0500

    Add about Us page

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -9,6 +9,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Search from "./pages/Search";
+import AboutUs from "./pages/AboutUs";
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />}/>
                 <Route path="/search" element={<Search />}/>
+                <Route path="/about-us" element={<AboutUs />}/>
                 <Route path="*" element={<Navigate to="/" />}/>
             </Routes>
         </Router>
