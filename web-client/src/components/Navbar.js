e5c41d2d4ee69fe5bf734d8e6ab798d287070095
Meixuan Chen <ellatudor1850@gmail.com>
2024-09-27 21:22:33 -0500

    Add about Us page

--- web-client/src/components/Navbar.js
+++ web-client/src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand">
         <img src={logo} alt="Logo" width="56" height="50" className="d-inline-block align-text-top"></img>
         Recipe Shop
         </a>
@@ -25,7 +25,7 @@ export default function Navbar() {
               About
             </a>
             <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">About us</a></li>
+              <li><Link to="/about-us"><a className="dropdown-item">About Us</a></Link></li>
               <li><a className="dropdown-item" href="https://developer.edamam.com/edamam-docs-recipe-api">Edamam API</a></li>
               <li><hr className="dropdown-divider"/></li>
               <li><a className="dropdown-item" href="https://github.com/njzfjiang/Recipe-Shop">GitHub Repo</a></li>
