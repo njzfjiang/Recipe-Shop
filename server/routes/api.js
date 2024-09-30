bb59cd310b432544341666740207550881b81eff
Troy Thomas <>
2024-09-30 18:41:35 -0500

    leaned up server.js and added api requests to api.js
    removed unnecessary code from server.js
    added api requests examples to api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+//e.g. GET message to 'localhost/api
 router.get("/", (req, res) => {
-    res.status(200).send("/api");
+    res.status(200).send("this url path is for api 'localhost/api/*'");
+});
+
+//e.g. POST message to 'localhost/api/register'
+router.post("/register", (req, res) => {
+    console.log(req.body);
+    //if register successful
+    res.status(200).send("registered");
+    //else
+    res.status(400).send("invalid username/password")
 });
 
 module.exports = router;
