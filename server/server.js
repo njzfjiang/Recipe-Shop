8a505af7dd712fd867836afde763bf11bdecf142
Troy Thomas <>
2024-09-30 18:21:24 -0500

    changes to README.md
    updated README.md in server and web-client with info to build web-client and run server

--- server/server.js
+++ server/server.js
@@ -12,16 +12,6 @@ app.use("/api", apiRouter);
 app.get("/", (req, res) => {
     console.log("Get /");
     res.sendFile(path.resolve(__dirname, "../web-client/build", "index.html"));
-    /*fs.readFile("../web-client/public/index.html", "utf-8", (err, data) => {
-        if(err){
-            console.log(err);
-            res.status(500).send("index not found");
-        }
-        else{
-            console.log(data);
-            res.status(200).send(data);
-        }
-    });*/
 });
 
 app.listen(PORT, () => {
