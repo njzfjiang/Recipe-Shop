3855b4e67cef3e7e0e91ef565058e8a875c887f3
Troy Thomas <>
2024-09-30 16:26:26 -0500

    created backend web server
    created express web server "server.js" with routes for api "api.js"

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 80;
+const apiRouter = require("./routes/api")
+const fs = require("node:fs");
+
+app.use("/api", apiRouter);
+
+app.get("/", (req, res) => {
+    console.log("Get /");
+    fs.readFile("./web-client/public/index.html", "utf-8", (err, data) => {
+        if(err){
+            console.log(err);
+            res.status(500).send("App.js not found");
+        }
+        else{
+            console.log(data);
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Server listening on port: ", PORT);
+});
