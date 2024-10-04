c78c9e14103c974090b05ace37ab583893b4c9cb
Meixuan Chen <ellatudor1850@gmail.com>
2024-10-03 19:53:58 -0500

    add tests for recipe page

--- web-client/src/pages/Recipe.js
+++ web-client/src/pages/Recipe.js
@@ -22,13 +22,12 @@ function Recipe () {
         })
 
         axios.get(url)
-            .then(response => {
-                if(response !== null){
+            .then( response =>  {
                 setRecipeData({
                     loading:false,
                     data: response.data,
                     error:false,})
-                }
+                
             })
             .catch((error) => {
             console.log(error);
@@ -51,7 +50,7 @@ function Recipe () {
     //render content if there is data.
     if(recipeData.data){
         let ingredientList = 
-        recipeData.data.recipe.ingredientLines.map((ingredient, key) =>
+        recipeData.data.recipe.ingredientLines.map((ingredient) =>
             <div className="mb-3">{ingredient}</div>
         );
 
@@ -60,7 +59,7 @@ function Recipe () {
                 <div className="row">
                     <div className="col">
                         <h2>{recipeData.data.recipe.label}</h2>
-                        <p>Recipe from {recipeData.data.recipe.source}</p>
+                        <p data-testid="source">Recipe from {recipeData.data.recipe.source}</p>
                         <img src={recipeData.data.recipe.image} alt="recipe"/>
                         
                     </div>
--- web-client/src/pages/Recipe.test.js
+++ web-client/src/pages/Recipe.test.js
@@ -1,32 +1,70 @@
 import Recipe from './Recipe';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import {Routes, Route, MemoryRouter } from 'react-router-dom';
 import { render, screen, cleanup, waitFor } from "@testing-library/react"
 import { fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
 import axios from 'axios';
-import MockAdapter from "axios-mock-adapter";
 
-window.alert = jest.fn();
+jest.mock('axios');
+
 afterEach(() => {
     jest.resetAllMocks();
+    cleanup
 });
 afterAll(() => {
     jest.clearAllMocks();
 });
 
 describe('Recipe page functionality', () => {
-    test('Search Page loaded correctly', async () => {
-        var mock = new MockAdapter(axios);
-        const data = { hits:[{recipe:
+    test('Recipe Page loaded correctly', async () => {
+        const data = {recipe:
             {uri:"recipe_12345",
             label:"tomato soup",
-            image:"https://cdn.loveandlemons.com/wp-content/uploads/2023/01/tomato-soup-723x1024.jpg"
-            }}]
-        }
-        
-        mock.onGet("http://localhost/api/recipe/search").reply(200, data);
-        
+            image:"https://cdn.loveandlemons.com/wp-content/uploads/2023/01/tomato-soup-723x1024.jpg",
+            source:"John Doe",
+            ingredientLines:["1 tomato","200 ml water"],
+            url:"https://cdn.loveandlemons.com/wp-content/uploads/2023/01/tomato-soup-723x1024.jpg"
+            }}
+       
+
+        axios.get.mockResolvedValue({data});
+
+        render(
+            <MemoryRouter initialEntries={["/recipes/12345"]}>
+                <Routes>
+                    <Route path="/recipes/12345" element={<Recipe />}>
+                    </Route>
+                </Routes>
+            </MemoryRouter>
+        );
+
+        await waitFor(()=> {
+            expect(screen.getByText('tomato soup')).toBeInTheDocument();
+            expect(screen.getByTestId('source')).toBeInTheDocument();
+            expect(screen.getByText('1 tomato')).toBeInTheDocument();
+            expect(screen.getByText('200 ml water')).toBeInTheDocument();
+            expect(screen.getByText('Search More recipes')).not.toBeDisabled();
         })
+        })
+    
+    test('Recipe page handles error', async () => {
+        const err = new Error('Server Error');
+        axios.get.mockRejectedValue(err);
+
+        render(
+            <MemoryRouter initialEntries={["/recipes/12345"]}>
+                <Routes>
+                    <Route path="/recipes/12345" element={<Recipe />}>
+                    </Route>
+                </Routes>
+            </MemoryRouter>
+        );
+
+        await waitFor(()=> {
+            expect(screen.getByText('An error occured, pleast try again later.')).toBeInTheDocument();
+        })
+    })
+
 })
