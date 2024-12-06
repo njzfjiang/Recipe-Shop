d7407707b59a499212d5fa9bd7875454c70b610c
Troy Thomas <>
2024-12-06 01:59:35 -0600

    Reapply "Merge branch 'web-client-upload' into dev"
    This reverts commit d97dabf9f5d68a281f9623a86d94989efde15268.

--- web-client/src/components/FavoriteListItem.js
+++ web-client/src/components/FavoriteListItem.js
@@ -4,12 +4,23 @@ import { Link } from "react-router-dom";
 const FavoriteListItem = (props) => {
     let recipeID = props.recipeID;
     let content = props.title;
-
-    return (
+    let source = props.source;
+    let html = ""
+    
+    if(source === "recipe-shop"){
+        html = 
+        <div className="p-3 mb-3 list-group-item list-group-item-action list-group-item-light">
+            <Link to={`/uploaded-recipes/${recipeID}`}>{content}</Link>
+        </div>
+    }
+    else{
+        html = 
         <div className="p-3 mb-3 list-group-item list-group-item-action list-group-item-light">
             <Link to={`/recipes/${recipeID}`}>{content}</Link>
         </div>
-    );
+    }
+
+    return html;
 }
 
 export default FavoriteListItem;
--- web-client/src/pages/MyRecipes.js
+++ web-client/src/pages/MyRecipes.js
@@ -1,25 +1,80 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
+import axios from 'axios'
+import UploadListItem from "../components/UploadListItem";
+import { Link } from 'react-router-dom';
 
 function MyRecipes () {
     const currUser = localStorage.getItem("user");
     const [loggedIn, setLoggedIn] = useState(false);
+    const [listData, setListData] = useState({
+        data:null,
+        error:false,
+        default:true
+    })
+
     let content = null;
-    let recipeList = null;
+    const [searchData, setSearchData] = useState('');
+    const [filterRecipes, setFilterRecipes] = useState([]);
     
+    const handleChange = (e) => {
+        const searchItem = e.target.value;
+        setSearchData(searchItem);
+
+        //if there is data, do the filtering.
+        if(listData.data !== null){
+            const filteredRecipes = listData.data.recipes.filter((recipe) => recipe.title.toLowerCase().includes(searchItem.toLowerCase()));
+            setFilterRecipes(filteredRecipes);
+        }
+    };
+
     useEffect( () => {
         if(!currUser){
           setLoggedIn(false);
         } else if (currUser !== null){
           setLoggedIn(true);
-        }
+          const url = "http://" + window.location.host + "/api/all-uploads/"+ localStorage.getItem("user");
+            axios.get(url)
+            .then(result => {
+                if(result.status === 200){
+                    setListData({
+                        data: result.data,
+                        error: false,
+                        default: false
+                    })
+                    setFilterRecipes(result.data.recipes)
+                }
+            })
+            .catch(error => {console.log(error);
+                setListData({
+                    data: null,
+                    error: true,
+                    default: false
+                })
+            });
+            }
     }, [currUser])
 
-    if(loggedIn === true){
-        content = <div className="p-3">
-            <input type="text" className="form-control mb-3" placeholder="Keyword"/>
-            {recipeList}
+
+    if(listData.data){
+        const recipeList = filterRecipes.map((item) => 
+            <div key={item._id}><UploadListItem recipeID={item._id} title={item.title}/></div> )
+
+        content = 
+        <div className="p-3">
+        <input type="text" className="form-control mb-3" placeholder="Keyword" value={searchData} onChange={handleChange}/>
+        {recipeList.length === 0 ?
+            <p className="text-center p-3">No recipes Found!</p>:
+            recipeList}
         </div>
+    }
+
+    if(listData.error){
+        content = <p data-testid="no-recipe-message" className="text-center p-3">No uploaded recipes found. You can upload recipes here:  <Link to="/upload">Upload Recipe</Link></p>
+    }
+
+    if(listData.default){
+        content = <p className="text-center p-3">Fetching Uploaded recipes...</p>
     }
 
     if(loggedIn === false){
